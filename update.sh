c8462f9d704a983c4ab0d83cab6b28616ef83782
flyash <fenxuehaiyan@qq.com>
2019-04-17 08:59:50 +0000

    Add another ad source & update rules

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ mkdir tmp
 cd tmp
 
 wget -O sr.conf https://raw.githubusercontent.com/h2y/Shadowrocket-ADBlock-Rules/master/sr_top500_banlist_ad.conf
+wget -O ad-another.conf https://cdn.jsdelivr.net/gh/neoFelhz/neohosts@gh-pages/basic/hosts
 
 # gw
 cat sr.conf | grep Proxy|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > gw
@@ -18,11 +19,18 @@ cat sr.conf | grep Reject|grep DOMAIN-SUFFIX|awk -F, '{print $2}' > ad
 # add custom ad hosts
 cat ../ad.conf >> ad
 
+# ad another
+cat ad-another.conf | grep 0.0.0.0|awk '{print $2}' > ad-another
+# add custom ad hosts
+cat ../ad.conf >> ad-another
+
 # Uniq and sort ad list
 sort -u -o ad ad
+sort -u -o ad-another ad-another
 
 # Allow ad in blank list
 comm -2 -3 ad ../ad_blank.conf > ../site/ad
+comm -2 -3 ad-another ../ad_blank.conf > ../site/ad-another
 
 cd ..
 rm -rf tmp
@@ -40,3 +48,4 @@ elif [[ "$OSTYPE" == "win32" ]]; then
     # windows
     ./v2sitedat32.exe -dat ./site.dat -dir ./site
 fi
+
